5aa4ffe31f1470ca6238bf1af86bbc8f9e5f7111
Raul Alcantara <ralcanta@mit.edu>
2020-08-06 21:49:12 -0400

    first commit

--- api/api.py
+++ api/api.py
@@ -1,6 +1,67 @@
-from flask import Flask
-app = Flask(__app__)
+from flask import Flask, request, Response, stream_with_context
+from flask_cors import CORS, cross_origin
 
-@app.route("/test")
+import cv2
+import base64
+# import requests
+app = Flask(__name__)
+CORS(app)
+color = True
+
+@app.route("/test", methods=['GET', 'POST'])
 def getTest():
-    return {"result": 2 + 2}
+    result = request.get_json()
+    return {"result": result['num']+1}
+
+@app.route("/changeColor")
+def changeColor():
+    global color
+    color = not color
+    return {"status": 200, "message": "Everything good here bruh"}
+@app.route('/image')
+def getImage():
+    path = "../videos/test_daily.mp4"
+    # path = 0
+    return Response(get_stream(path), mimetype="text")
+# @stream_with_context
+def get_stream(path):
+    # for i in range(100):
+    #     yield str(i)
+    # return
+    cap = cv2.VideoCapture(path)
+    # i = 0
+    w  = 320 #int(vid_obj.get(cv2.CAP_PROP_FRAME_WIDTH) /3 ) # dividing because we will be resizing!
+    h = 180 #int(vid_obj.get(cv2.CAP_PROP_FRAME_HEIGHT) /3 )# float
+    while True:
+        ret, frame = cap.read()
+        # ret, frame = cap.read()
+        # ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv2.resize(frame, dsize=(w, h))#,fx=1/3, fy=1/3)
+        # frame = cv2.color(frame, cv2.CV2)
+        if not color:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        encoded_frame = get_encode(frame) # { "frame", }
+        # print(frame.shape)
+        # print("encoded frame:")
+        # print(encoded_frame)
+        # print("\n")
+        # i+=1
+        # print("sedning frame #"+str(i))
+        yield "data:image/png;base64," + encoded_frame + "#"
+        # yield "#"
+        # break
+        
+
+    cap.release()
+def get_encode(frame):
+    
+    retval, buffer = cv2.imencode('.jpg', frame)
+    jpg_as_text = base64.b64encode(buffer)
+    # print(jpg_as_text)
+    x = jpg_as_text.decode("utf-8") #delete the b'' elements
+    return x #f"data:image/png;base64,{x}"
+
+
--- src/App.js
+++ src/App.js
@@ -2,25 +2,204 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      num : 0,
+      image_source: "",
+      color: true,
+    }
+    this.pushFrames = this.pushFrames.bind(this);
+  }
+  changeColor = ()=>{
+    console.log("pressed!")
+    // changing to 
+    fetch('/changeColor')
+    .then(res=>res.json())
+    .then(result =>{
+      console.log("Returned post with status "+ result.status);
+      console.log("Message: "+ result.message);
+    })
+    this.setState({
+      color: !this.state.color,
+    })
+    return
+    let body = JSON.stringify(this.state);
+    let params = {
+      method: "POST",
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: body,
+    }
+    fetch('/test', params)
+    .then(res => res.body.getReader())
+    .then(reader =>{
+      while (true){
+        const {done, value} = reader.read();
+        if (done){
+          break;
+        }
+        this.setState({
+          num: value.result
+        })
+      }
+    })
+    // .then(data =>{
+    //   console.log(data)
+    //   this.setState({
+    //       num: data.result
+    //   })
+    // })
+  }
+  async pushFrames(videoUrl, fps=25){
+      return new Promise(async (resolve) => {
+    
+        // fully download it first (no buffering):
+        let videoBlob = await fetch(videoUrl).then(r => r.blob());
+        let videoObjectUrl = URL.createObjectURL(videoBlob);
+        let video = document.createElement("video");
+    
+        let seekResolve;
+        video.addEventListener('seeked', async function() {
+          if(seekResolve) seekResolve();
+        });
+    
+        video.src = videoObjectUrl;
+    
+        // workaround chromium metadata bug (https://stackoverflow.com/q/38062864/993683)
+        while((video.duration === Infinity || isNaN(video.duration)) && video.readyState < 2) {
+          await new Promise(r => setTimeout(r, 1000));
+          video.currentTime = 10000000*Math.random();
+        }
+        let duration = video.duration;
+    
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d');
+        let [w, h] = [video.videoWidth, video.videoHeight]
+        canvas.width =  w;
+        canvas.height = h;
+    
+        let frames = [];
+        let interval = 1 / fps;
+        let currentTime = 0;
+    
+        while(currentTime < duration) {
+          video.currentTime = currentTime;
+          await new Promise(r => seekResolve=r);
+    
+          context.drawImage(video, 0, 0, w, h);
+          let base64ImageData = canvas.toDataURL();
+          frames.push(base64ImageData);
+    
+          currentTime += interval;
+        }
+        resolve(frames);
+      });
+  }
+  getImage = ()=>{
+    console.log("getting image...")
+    fetch("/image")
+    .then(res => res.json())
+    .then(result => {
+        console.log(result)
+        this.setFrame(result.frame)
+    })
+  }
+  getImage2 = ()=>{
+    let that = this;
+    console.log("getting image...")
+    fetch("/image")
+    .then(res => res.body.getReader())
+    .then(async function(reader){ 
+      let runs = 0;
+      let full = "";
+      let took = 0;
+      while (true){
+        // console.log(reader.read())
+        const {done, value} = await reader.read(); //value is Uint8Array
+        if (done){
+          console.log("it's done!")
+          break;
+        }
+        let ImageDataArrayBuffer = value //new Uint8Array(value); //from https://medium.com/@koteswar.meesala/convert-array-buffer-to-base64-string-to-display-images-in-angular-7-4c443db242cd
+                                                          // unclear why I don't need to do Step #3 from ^^^
+        const STRING_CHAR = ImageDataArrayBuffer.reduce((data, byte)=> {
+          return data + String.fromCharCode(byte);
+          }, ''); //binary string
+        full += STRING_CHAR;
+        took +=1
+        if (STRING_CHAR.endsWith("#")){
+          // that.setFrame("data:image/png;base64,"+full.slice(0, full.length-1));
+          runs+=1
+          console.log("frame #"+runs+ ", took "+took+" frames")
+          took = 0;
+          that.setState({
+            image_source: full.slice(0, full.length-1),
+          })
+          full = ""
+        }
+      } 
+      // that.setFrame("data:image/png;base64,"+full)
+
+    })
+  }
+  isBase64 =(str) =>{
+    var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    return base64regex.test(str);
+    if (str ==='' || str.trim() ===''){ return false; }
+    try {
+        return btoa(atob(str)) == str;
+    } catch (err) {
+        return false;
+    }
+  }
+  setFrame = (frame)=>{
+    this.setState({
+      image_source: frame,
+    })
+  }
+  // getStream =()=>{
+  //   this.timer = setInterval(()=>{
+  //     this.getImage();
+  //   }, 1000)
+  // }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <button onClick={()=>this.pushFrames("videos/test_daily.mp4")}>
+            Push frames
+          </button> */}
+          <button onClick={this.changeColor}>
+            Change Color
+          </button>
+          <button onClick={()=>this.getImage2()}>
+            Wanna see yourself?
+          </button>
+          {
+            this.state.image_source.length > 0 &&
+            <img 
+            src ={`${this.state.image_source}`}
+            alt="this is me"
+            with={500}
+            height={500}
+            />
+          }
+
+          {this.state.num}
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
